29ce35284324ee41f1aa77043f079bc7fc7d52fe
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-10-15 00:17:07 -0400

    Handled second confirmation in coach onboarding process

--- coach-me/src/components/coach/coachDashboard/CoachDashboard.js
+++ coach-me/src/components/coach/coachDashboard/CoachDashboard.js
@@ -41,60 +41,84 @@ const CoachDashboard = () => {
   };
 
   return (
-    <div className='dashboard'>
-      <div className='side-one'>
-        <h1>Let's get you started!</h1>
-        <p>
-          Objectively deliver vertical internal or "organic" sources vis-a-vis
-          turnkey oppurtunities
-        </p>
-        <img src='https://i.imgur.com/7YHZ8gM.jpg' alt='Placeholder' />
-      </div>
-      {checkOne ? (
-        <div className='side-two'>
-          <h1>Confirm Number</h1>
-          <p>Enter 4 digit code that was sent to +1 {number}.</p>
-          <input
-            type='text'
-            value={verifyNumber}
-            onChange={handleInputTwo}
-            maxLength='4'
-          />
-
-          <div className='flex'>
-            <p>Change phone number?</p>
-            <p>Resend Code</p>
+    <>
+      {checkTwo ? (
+        <div className='welcome'>
+          <div className='side-one'>
+            <img src='https://i.imgur.com/7YHZ8gM.jpg' alt='Placeholder' />
           </div>
-          {verifyNumber ? (
-            <div className='send-btn'>Confirm</div>
-          ) : (
-            <div className='send-btn-disabled'>Confirm</div>
-          )}
+          <div className='side-two'>
+            <h1>Welcome to CoachMe!</h1>
+            <p>
+              Efficiently facilitate cutting-edge e-tailers without just in time
+              value.
+            </p>
+            <div className='continue-btn' onClick={handleCheckTwo}>
+              Continue
+            </div>
+          </div>
         </div>
       ) : (
-        <div className='side-two'>
-          <h1>Phone Number</h1>
-          <p>Enter a phone number below. We will send you a code to verify.</p>
-          <input
-            type='number'
-            placeholder='Mobile number'
-            value={number}
-            onChange={handleInput}
-            maxLength='10'
-          />
+        <div className='dashboard'>
+          <div className='side-one'>
+            <h1>Let's get you started!</h1>
+            <p>
+              Objectively deliver vertical internal or "organic" sources
+              vis-a-vis turnkey oppurtunities
+            </p>
+            <img src='https://i.imgur.com/7YHZ8gM.jpg' alt='Placeholder' />
+          </div>
+          {checkOne ? (
+            <div className='side-two'>
+              <h1>Confirm Number</h1>
+              <p>Enter 4 digit code that was sent to +1 {number}.</p>
+              <input
+                type='text'
+                value={verifyNumber}
+                onChange={handleInputTwo}
+                maxLength='4'
+              />
 
-          {number ? (
-            <div className='send-btn' onClick={handleCheckOne}>
-              Send Code
+              <div className='flex'>
+                <p>Change phone number?</p>
+                <p>Resend Code</p>
+              </div>
+              {verifyNumber ? (
+                <div className='send-btn' onClick={handleCheckTwo}>
+                  Confirm
+                </div>
+              ) : (
+                <div className='send-btn-disabled'>Confirm</div>
+              )}
             </div>
           ) : (
-            <div className='send-btn-disabled'>Send Code</div>
+            <div className='side-two'>
+              <h1>Phone Number</h1>
+              <p>
+                Enter a phone number below. We will send you a code to verify.
+              </p>
+              <input
+                type='number'
+                placeholder='Mobile number'
+                value={number}
+                onChange={handleInput}
+                maxLength='10'
+              />
+
+              {number ? (
+                <div className='send-btn' onClick={handleCheckOne}>
+                  Send Code
+                </div>
+              ) : (
+                <div className='send-btn-disabled'>Send Code</div>
+              )}
+            </div>
           )}
+          {/* <h1>Clients</h1>
+      {users && users.map(client => <ClientCard client={client} />)} */}
         </div>
       )}
-      {/* <h1>Clients</h1>
-      {users && users.map(client => <ClientCard client={client} />)} */}
-    </div>
+    </>
   );
 };
 
