39c9e67fb0d5abb3456ef42e729143785a07dcd2
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-10-15 00:00:52 -0400

    Handled first confirmation in coach onboarding process

--- coach-me/src/components/coach/coachDashboard/CoachDashboard.js
+++ coach-me/src/components/coach/coachDashboard/CoachDashboard.js
@@ -8,6 +8,9 @@ import ClientCard from './clientsList/ClientCard';
 const CoachDashboard = () => {
   const [users, setUsers] = useState();
   const [number, setNumber] = useState();
+  const [verifyNumber, setVerifyNumber] = useState('');
+  const [checkOne, setCheckOne] = useState(false);
+  const [checkTwo, setCheckTwo] = useState(false);
 
   useEffect(() => {
     // Get request to airtable endpoint with api key appended to the end of url
@@ -21,8 +24,20 @@ const CoachDashboard = () => {
 
   console.log(users);
 
-  const handleChange = e => {
+  const handleInput = e => {
     setNumber(e.target.value);
+  };
+
+  const handleInputTwo = e => {
+    setVerifyNumber(e.target.value);
+  };
+
+  const handleCheckOne = () => {
+    setCheckOne(true);
+  };
+
+  const handleCheckTwo = () => {
+    setCheckTwo(true);
   };
 
   return (
@@ -35,22 +50,48 @@ const CoachDashboard = () => {
         </p>
         <img src='https://i.imgur.com/7YHZ8gM.jpg' alt='Placeholder' />
       </div>
-      <div className='side-two'>
-        <h1>Phone Number</h1>
-        <p>Enter a phone number below. We will send you a code to verify.</p>
-        <input
-          type='text'
-          placeholder='Mobile number'
-          value={number}
-          onChange={handleChange}
-        />
+      {checkOne ? (
+        <div className='side-two'>
+          <h1>Confirm Number</h1>
+          <p>Enter 4 digit code that was sent to +1 {number}.</p>
+          <input
+            type='text'
+            value={verifyNumber}
+            onChange={handleInputTwo}
+            maxLength='4'
+          />
 
-        {number ? (
-          <div className='send-btn'>Send Code</div>
-        ) : (
-          <div className='send-btn-disabled'>Send Code</div>
-        )}
-      </div>
+          <div className='flex'>
+            <p>Change phone number?</p>
+            <p>Resend Code</p>
+          </div>
+          {verifyNumber ? (
+            <div className='send-btn'>Confirm</div>
+          ) : (
+            <div className='send-btn-disabled'>Confirm</div>
+          )}
+        </div>
+      ) : (
+        <div className='side-two'>
+          <h1>Phone Number</h1>
+          <p>Enter a phone number below. We will send you a code to verify.</p>
+          <input
+            type='number'
+            placeholder='Mobile number'
+            value={number}
+            onChange={handleInput}
+            maxLength='10'
+          />
+
+          {number ? (
+            <div className='send-btn' onClick={handleCheckOne}>
+              Send Code
+            </div>
+          ) : (
+            <div className='send-btn-disabled'>Send Code</div>
+          )}
+        </div>
+      )}
       {/* <h1>Clients</h1>
       {users && users.map(client => <ClientCard client={client} />)} */}
     </div>
