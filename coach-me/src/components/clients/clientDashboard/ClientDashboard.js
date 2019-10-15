fe2cb007980ce0206d444eb3b3c5a5326a364022
masonkarsevar <masonkarsevar@gmail.com>
2019-10-15 12:47:56 -0700

    added extra axios call to client dashboard for client checkin array

--- coach-me/src/components/clients/clientDashboard/ClientDashboard.js
+++ coach-me/src/components/clients/clientDashboard/ClientDashboard.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import HealthMetric from '../healthMetrics/HealthMetric';
 
 import './clientDashboard.scss';
 
 const ClientDashboard = () => {
-    const [userId, setUserId] = useState('recAY9lIrdsr2rDYT');
+    const [userId, setUserId] = useState('recYrQGcD4LSJmPjk');
+    const [checkinList, setCheckinList] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.airtable.com/v0/app3X8S0GqsEzH9iW/Master/${userId}?api_key=keyfahybUIpBkegFv`
+            )
+            .then(results => {
+                console.log('from master table', results.data);
+                setCheckinList(results.data.fields['weekly check-ins 2']);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
 
     return (
         <div>
             <h2>Welcome to client dashboard</h2>
-            <HealthMetric userId={userId} />
+            <HealthMetric checkinList={checkinList} userId={userId} />
         </div>
     );
 };
--- coach-me/src/components/clients/healthMetrics/HealthMetric.js
+++ coach-me/src/components/clients/healthMetrics/HealthMetric.js
@@ -6,20 +6,32 @@ import './healthMetrics.scss';
 const HealthMetric = props => {
     const [clientData, setClientData] = useState();
 
-    console.log(props.userId);
+    console.log(props);
+
+    // console.log(props.userId);
+
+    // for (let i = 0; i < props.checkinList.length; i++) {
+    //     console.log(props.checkinList[i]);
+    // }
+
+    const array = [1, 3, 4, 5, 6];
 
     useEffect(() => {
-        axios
-            .get(
-                `https://api.airtable.com/v0/app3X8S0GqsEzH9iW/Check-ins/${props.userId}?api_key=keyfahybUIpBkegFv`
-            )
-            .then(results => {
-                console.log(results);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    });
+        for (let i = 0; i < props.checkinList.length; i++) {
+            console.log(props.checkinList[i]);
+            axios
+                .get(
+                    `https://api.airtable.com/v0/app3X8S0GqsEzH9iW/Check-ins/${props.checkinList[i]}?api_key=keyfahybUIpBkegFv`
+                )
+                .then(results => {
+                    console.log(results.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }, [props.checkinList]);
+
     return (
         <div>
             <h1>Metrics</h1>
