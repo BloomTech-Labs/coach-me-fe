18bb800a776d4b278ef34e2b636841ccc36da904
masonkarsevar <masonkarsevar@gmail.com>
2019-10-16 08:10:20 -0700

    toggle between health metric and health metric cards

--- coach-me/src/components/clients/healthMetrics/HealthMetricCards.js
+++ coach-me/src/components/clients/healthMetrics/HealthMetricCards.js
@@ -1,31 +1,17 @@
 import React from 'react';
 
 function HealthMetricCards(props) {
-    // console.log(props.record);
-    const recordInfo = props.record;
+    console.log(props);
 
-    console.log('from recordInfo', recordInfo);
+    const handleClick = () => {
+        props.setToggleHistory(false);
+    };
+
     return (
-        <div className='health-card'>
-            <h4>Checkin Date: {recordInfo.fields['Date of Check-in']}</h4>
-            <h4>
-                Blood Pressure:{' '}
-                {recordInfo.fields['Most recent blood pressure?']
-                    ? recordInfo.fields['Most recent blood pressure?']
-                    : 'None'}
-            </h4>
-            <h4>
-                Weight:{' '}
-                {recordInfo.fields['Most recent weight? (pounds)']
-                    ? recordInfo.fields['Most recent weight? (pounds)']
-                    : 'None'}
-            </h4>
-            <h4>
-                Blood Glucose Level:{' '}
-                {recordInfo.fields['Most recent blood glucose level?']
-                    ? recordInfo.fields['Most recent blood glucose level?']
-                    : 'None'}
-            </h4>
+        <div className='metric-container'>
+            <button className='back-button' onClick={() => handleClick()}>
+                Back
+            </button>
         </div>
     );
 }
