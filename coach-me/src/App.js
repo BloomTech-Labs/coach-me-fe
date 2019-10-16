b884134090e4422f7294094282ddc19e46f70a53
Lambda_School_Loaner_93 <antoniounivers@gmail.com>
2019-10-16 14:00:03 -0400

    fixed changes

--- coach-me/src/App.js
+++ coach-me/src/App.js
@@ -4,24 +4,21 @@ import './App.css';
 //Components
 
 import LoginClient from './components/clients/loginClient/LoginClient'
-import RegisterClient from './components/clients/registerClient/RegisterClient'
+// import RegisterClient from './components/clients/registerClient/RegisterClient'
 import PrivateRoute from './components/utils/PrivateRoute';
 import HealthMetricForm from './components/clients/healthMetricForm/HealthMetricForm';
 import Routes from './components/utils/Routes';
-import ClientLogin from './components/clients/loginClient/ClientLogin'
+// import ClientLogin from './components/clients/loginClient/ClientLogin'
 import { Provider } from 'react-redux';
 import store from './store';
 
 function App() {
   return (
-    // <Provider store={store}>
     <div className='App'>
       <LoginClient/>
-      {/* <RegisterClient/> */}
-      {/* <Routes /> */}
       <HealthMetricForm />
     </div>
-    // </Provider>
+
   );
   }
 
