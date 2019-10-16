1fd27ce2a1a925330103b4de0a1f80c9ad579b91
jjgorton <jjgorton@gmail.com>
2019-10-16 18:21:49 -0400

    added action types, action, and cases to reducer

--- coach-me/src/reducers/clientReducer.js
+++ coach-me/src/reducers/clientReducer.js
@@ -1,42 +1,64 @@
 import {
-    GET_CLIENTS,
-    ADD_CLIENT,
-    DELETE_CLIENT,
-    CLIENTS_ERROR,
-    UPDATE_METRIC_START,
-    UPDATE_METRIC_SUCCESS,
-    UPDATE_METRIC_FAILURE
+  GET_CLIENTS_START,
+  GET_CLIENTS_SUCCESS,
+  GET_CLIENTS_FAILURE,
+  GET_CLIENTS,
+  ADD_CLIENT,
+  DELETE_CLIENT,
+  CLIENTS_ERROR,
+  UPDATE_METRIC_START,
+  UPDATE_METRIC_SUCCESS,
+  UPDATE_METRIC_FAILURE
 } from '../actions/types';
 
 const initialState = {
-    records: null,
-    isfetching: false,
-    error: ''
+  clientInfo: {},
+  gettingClient: false,
+  records: null,
+  isfetching: false,
+  error: ''
 };
 
 export default (state = initialState, action) => {
-    console.log('These are teh actions', action.payload);
-    switch (action.type) {
-        case UPDATE_METRIC_START:
-            return {
-                ...state,
-                isfetching: true,
-                error: ''
-            };
-        case UPDATE_METRIC_SUCCESS:
-            return {
-                ...state,
-                isfetching: false,
-                records: { ...action.payload },
-                error: ''
-            };
-        case UPDATE_METRIC_FAILURE:
-            return {
-                ...state,
-                isfetching: false,
-                err: action.payload
-            };
-        default:
-            return state;
-    }
+  console.log('These are teh actions', action.payload);
+  switch (action.type) {
+    case GET_CLIENTS_START:
+      return {
+        ...state,
+        gettingClient: true
+      };
+    case GET_CLIENTS_SUCCESS:
+      return {
+        ...state,
+        gettingClient: false,
+        clientInfo: action.payload
+      };
+    case GET_CLIENTS_FAILURE:
+      return {
+        ...state,
+        gettingClient: false,
+        error: action.payload
+      };
+    case UPDATE_METRIC_START:
+      return {
+        ...state,
+        isfetching: true,
+        error: ''
+      };
+    case UPDATE_METRIC_SUCCESS:
+      return {
+        ...state,
+        isfetching: false,
+        records: { ...action.payload },
+        error: ''
+      };
+    case UPDATE_METRIC_FAILURE:
+      return {
+        ...state,
+        isfetching: false,
+        err: action.payload
+      };
+    default:
+      return state;
+  }
 };
