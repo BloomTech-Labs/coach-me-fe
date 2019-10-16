b475718aa3b205250cea4c1aee07e754dc9d2f3b
Lambda_School_Loaner_93 <antoniounivers@gmail.com>
2019-10-16 16:23:58 -0400

    client info added to reducer

--- coach-me/src/components/clients/loginClient/LoginClient.js
+++ coach-me/src/components/clients/loginClient/LoginClient.js
@@ -2,64 +2,83 @@ import React from 'react';
 import { auth } from '../../../firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import './loginClient.scss';
-import axios from 'axios'
+
+import { connect } from 'react-redux';
+import {getClientInfo} from '../../../actions/clientActions'
 auth().useDeviceLanguage();
 auth().settings.appVerificationDisabledForTesting = true;
+//925-639-1639
 
-const uiconfig = {
-    signInFlow: 'popup',
-    signInSuccessUrl: 'http://localhost:3000/',
-    signInOptions: [
-        {
-            provider: auth.PhoneAuthProvider.PROVIDER_ID,
-            recaptchaParameters: {
-                size: 'invisible'
+
+
+
+
+const LoginClient = (props) => {
+
+
+    const uiconfig = {
+        signInFlow: 'popup',
+        signInSuccessUrl: 'http://localhost:3000/',
+        signInOptions: [
+            {
+                provider: auth.PhoneAuthProvider.PROVIDER_ID,
+                recaptchaParameters: {
+                    size: 'invisible'
+                }
             }
-        }
-      
-      
-      
-  ],
-
-  callbacks:{
-    signInSuccessWithAuthResult: function (){
-      const token = auth().currentUser.getIdToken()
-      const phone = auth().currentUser.phoneNumber.toString().slice(2,)
-      
+          
+          
+          
+      ],
     
-     token.then( (res) =>{
-       localStorage.setItem('token',res)
-       localStorage.setItem('phone',phone)
-       
-       
-     }).then( ()=>{
-      const num = localStorage.getItem('phone')
-      console.log(num)
-      axios.get(`https://api.airtable.com/v0/appcGDj4SuiTu3Nte/Intake?api_key=keytu1to8j0ODW8sD&maxRecords=1&view=Grid%20view&phone=${num}`).then( res =>{
-        console.log(res)
-      }).catch( err =>{
-        console.log(err)
-      })
-     })
+      callbacks:{
+        signInSuccessWithAuthResult: function (){
+          const token = auth().currentUser.getIdToken()
+          const phone = auth().currentUser.phoneNumber.toString().slice(2,)
+          
+        
+         token.then( (res) =>{
+           localStorage.setItem('token',res)
+           localStorage.setItem('phone',phone)
+           
+           
+         }).then( ()=>{
+          const num = localStorage.getItem('phone')
+          props.getClientInfo(num)
+          
+         })
+         
+        },
+        
+        
+       }
+      
      
-    },
-    
-    
-   }
-  
- 
-  
-}
+      
+    }
+    console.log(props.clientinfo)
 
-
-
-const LoginClient = () => {
     return (
         <div className='auth-container'>
             <h1>Coach Me</h1>
             <StyledFirebaseAuth uiConfig={uiconfig} firebaseAuth={auth()} />
         </div>
     );
+    
 };
 
-export default LoginClient;
+
+
+
+const mapStatetoProps = state => {
+    console.log('App.js', state)
+    return {
+        clientinfo : state.clientinfio
+    }
+}
+
+export default connect(
+    mapStatetoProps,
+    { getClientInfo }
+)(LoginClient);
+
