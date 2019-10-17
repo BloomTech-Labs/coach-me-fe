7c1d9a8b17ed87751eeb4acd50a8092af7012f58
Nick-Ballenger <nickster214@gmail.com>
2019-10-17 13:25:12 -0600

    Initial metric form functionality complete

---
+++ coach-me/src/components/clients/healthMetricForm/HealthMetricForm.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateMetric } from '../../../actions/clientActions';
+
+function HealthMetricForm(props) {
+  // Implements Redux
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  const [bpOver, setBpOver] = useState();
+  const [bpUnder, setBpUnder] = useState();
+  const [bS, setBS] = useState();
+  const [weight, setWeight] = useState();
+  const [metrics, setMetrics] = useState();
+
+  const handleInputChange = e => {
+    e.preventDefault();
+    setBS(e.target.value);
+  };
+  const handleInputChange2 = e => {
+    e.preventDefault();
+    setBpOver(e.target.value);
+  };
+  const handleInputChange3 = e => {
+    e.preventDefault();
+    setBpUnder(e.target.value);
+  };
+  const handleInputChange4 = e => {
+    e.preventDefault();
+    setWeight(e.target.value);
+  };
+
+  useEffect(() => {
+    setMetrics({
+      records: [
+        {
+          id: 'reck71PQQtBHkbNIF',
+          fields: {
+            Client_Name: ['rec43ppgrbQld6xPJ'],
+            Date_time: null,
+            Blood_pressure_over: parseInt(bpOver),
+            Blood_pressure_under: parseInt(bpUnder),
+            Blood_sugar: parseInt(bS),
+            Weight: parseInt(weight)
+          }
+        }
+      ]
+    });
+    console.log('whats being submitted', metrics);
+  }, [bpOver, bpUnder, bS, weight]);
+
+  const submitNewMetric = e => {
+    e.preventDefault();
+    dispatch(updateMetric(metrics));
+  };
+  return (
+    <div className='metric-form-wrapper'>
+      <h1>Today's Numbers</h1>
+      <form onSubmit={submitNewMetric}>
+        <div classname='input-label'>
+          <img></img>
+          <h3>Fasting Blood Glucose</h3>
+        </div>
+        <input
+          className='metric-input'
+          onChange={handleInputChange}
+          type='integer'
+          value={bS}
+          name='Blood_sugar'
+        />
+        <p>mg/dL</p>
+        <div classname='input-label'>
+          <img></img>
+          <h3>Weight</h3>
+        </div>
+        <input
+          className='metric-input'
+          onChange={handleInputChange4}
+          type='integer'
+          value={weight}
+          name='Weight'
+        />
+        <p>lbs</p>
+        <div classname='input-label'>
+          <img></img>
+          <h3>Blood Pressure</h3>
+        </div>
+        <div className='blood-pressure-container'>
+          <input
+            className='metric-input'
+            onChange={handleInputChange2}
+            type='integer'
+            value={bpOver}
+            name='Blood_pressure_over'
+          />
+          <p>/</p>
+          <input
+            className='metric-input'
+            onChange={handleInputChange3}
+            type='integer'
+            value={bpUnder}
+            name='Blood_pressure_under'
+          />
+          <p>mmHg</p>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+export default HealthMetricForm;
