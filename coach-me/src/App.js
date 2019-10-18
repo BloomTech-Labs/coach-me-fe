7c7aaa87befac1c59a0109b6bf5e324be03f1537
Nick-Ballenger <nickster214@gmail.com>
2019-10-18 14:24:37 -0600

    Merge conflict fixed in app

--- coach-me/src/App.js
+++ coach-me/src/App.js
@@ -8,22 +8,12 @@ import Routes from './components/utils/Routes';
 
 
 function App() {
-<<<<<<< HEAD
   return (
     <div className='App'>
       <Routes/>
    
     </div>
   );
-=======
-    return (
-        <div className='App'>
-            {/* <LoginClient /> */}
-            <Routes />
-            <HealthMetricForm />
-        </div>
-    );
->>>>>>> ae5a6b719b4fdc1e9e97f306ad10e7823ac737db
 }
 
 export default App;
