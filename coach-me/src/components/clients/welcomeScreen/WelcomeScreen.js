63e6307a4ca55dc9ab1c9855309f6e1189e40a02
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-10-25 12:15:56 -0400

    Fixed bug where client information was not being pulled into welcome component

--- coach-me/src/components/clients/welcomeScreen/WelcomeScreen.js
+++ coach-me/src/components/clients/welcomeScreen/WelcomeScreen.js
@@ -4,35 +4,42 @@ import axios from 'axios';
 import './welcomeScreen.scss';
 
 const WelcomeScreen = () => {
-    const [coach, setCoach] = useState('Karin');
+    const [coach, setCoach] = useState();
     const state = useSelector(state => state);
     const dispatch = useDispatch();
 
     useEffect(() => {
         axios
             .get(
-                'https://api.airtable.com/v0/app3X8S0GqsEzH9iW/Coaches?api_key=keyfahybUIpBkegFvs'
+                `https://coach-me-backend.herokuapp.com/clientRoute/getCoachInfo`,
+                {
+                    headers: {
+                        Authorization: sessionStorage.getItem('token')
+                    }
+                }
             )
             .then(res => setCoach(res.data))
             .catch(err => console.log(err.message));
     }, []);
 
+    console.log(coach);
+    console.log('state info', state);
     return (
         <div className='welcomeScreen'>
-            <h1>Welcome to CoachMe, {coach}!</h1>
+            <h1>Welcome to CoachMe, {state.clientinfo.name}!</h1>
             <img
-                src='https://dl.airtable.com/.attachments/2964a7624923f374610c1b583a7edc24/3b8b5096/Karin_bitmoji.jpeg'
+                src={coach && coach.coachObject.coachUrl}
                 alt='Current Coach'
             />
 
             <div className='header2'>
-                <h1>{coach}</h1>
+                <h1>{coach && coach.coachObject.coachName}</h1>
                 <p>Your Health Coach</p>
             </div>
 
             <p className='footerText'>
-                {coach} is here to support you in your health goals. Expect a
-                text message from your coach soon!
+                is here to support you in your health goals. Expect a text
+                message from your coach soon!
             </p>
         </div>
     );
