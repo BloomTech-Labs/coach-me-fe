f6961932dcf282bf19dcd650848af3a5cfbbaf2f
Pat Palmerston <patpalmerston@gmail.com>
2019-11-01 08:57:45 -0700

    updating redux for coaches and auth

--- coach-me/src/actions/authActions.js
+++ coach-me/src/actions/authActions.js
@@ -1,31 +1,30 @@
-import axios from 'axios'
+import axios from 'axios';
 import {
     REGISTER_START,
     REGISTER_SUCCESS,
     REGISTER_FAIL,
-    CLIENT_LOADED,
-    AUTH_ERROR,
+    // CLIENT_LOADED,
+    // AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGIN_START,
-    LOGOUT,
-    CLEAR_ERRORS,
-    GET_RECORDS_START ,
- GET_RECORDS_SUCCESS,
- GET_RECORDS_FAILURE
+    // LOGOUT,
+    // CLEAR_ERRORS,
+    GET_RECORDS_START,
+    GET_RECORDS_SUCCESS,
+    GET_RECORDS_FAILURE
 } from './types';
 
-
 export const registerCoach = register => dispatch => {
-    const creds = register.records[0].fields
+    const creds = register.records[0].fields;
     dispatch({ type: REGISTER_START });
     axios
         .post(
-            `https://coach-me-backend.herokuapp.com/coachRoute/register `,creds
-           
+            `${process.env.REACT_APP_BACK_END_URL}/coachRoute/newRegister `,
+            creds
         )
         .then(res => {
-            console.log(res)
+            console.log(res);
             dispatch({
                 type: REGISTER_SUCCESS,
                 payload: res.data
@@ -39,21 +38,15 @@ export const registerCoach = register => dispatch => {
         });
 };
 
-
 export const loginCoach = creds => dispatch => {
     dispatch({ type: LOGIN_START });
-    axios
-        .post(
-            `https://coach-me-backend.herokuapp.com/coachRoute/login`,
-            creds,
-           
-        )
+    return axios
+        .post(`${process.env.REACT_APP_BACK_END_URL}/coachRoute/login`, creds)
         .then(res => {
-            console.log(res)
-            localStorage.setItem('token',res.data.token)
+            console.log(res);
+            localStorage.setItem('token', res.data.token);
             dispatch({
-                type: LOGIN_SUCCESS,
-               
+                type: LOGIN_SUCCESS
             });
         })
         .catch(err => {
@@ -64,28 +57,25 @@ export const loginCoach = creds => dispatch => {
         });
 };
 
-export const getClients= (token) => dispatch => {
-    const headers = {Authorization:token}
+export const getClients = token => dispatch => {
+    const headers = { Authorization: token };
     dispatch({ type: GET_RECORDS_START });
     axios
-        .get(
-            `https://coach-me-backend.herokuapp.com/coachRoute/getPatients`,{headers:headers}
-        
-           
-        )
+        .get(`${process.env.REACT_APP_BACK_END_URL}/coachRoute/getPatients`, {
+            headers: headers
+        })
         .then(res => {
-            console.log(res.data.patientList)
-            
+            console.log(res.data.patientList);
+
             dispatch({
                 type: GET_RECORDS_SUCCESS,
-               payload: res.data.patientList
+                payload: res.data.patientList
             });
         })
         .catch(err => {
             dispatch({
-                type: GET_RECORDS_SUCCESS,
+                type: GET_RECORDS_FAILURE,
                 payload: err.message
             });
         });
 };
-
--- coach-me/src/reducers/authReducer.js
+++ coach-me/src/reducers/authReducer.js
@@ -1,19 +1,43 @@
-// import {
-//   REGISTER_SUCCESS,
-//   REGISTER_FAIL,
-//   CLIENT_LOADED,
-//   AUTH_ERROR,
-//   LOGIN_SUCCESS,
-//   LOGIN_FAIL,
-//   LOGOUT,
-//   CLEAR_ERRORS
-// } from '../actions/types';
+import {
+    // REGISTER_START,
+    // REGISTER_SUCCESS,
+    // REGISTER_FAIL,
+    // CLIENT_LOADED,
+    // AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGIN_START
+    // LOGOUT,
+    // CLEAR_ERRORS,
+    // GET_RECORDS_START,
+    // GET_RECORDS_SUCCESS,
+    // GET_RECORDS_FAILURE
+} from '../actions/types';
 
-// const intialState = {};
+const initialState = {
+    loggingIn: false,
+    isfetching: false,
+    error: null
+};
 
-// export default (state = initialState, action) => {
-// 	switch (action.type) {
-// 		default:
-// 			return state;
-// 	}
-// };
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN_START:
+            return {
+                ...state,
+                loggingIn: true
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                loggingIn: false
+            };
+        case LOGIN_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
