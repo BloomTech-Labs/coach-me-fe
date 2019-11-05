5b4ee77043ac7c492642c750bc749a0f0fb98e57
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-11-05 12:45:40 -0500

    Changed date inputs to select elements

--- coach-me/src/components/coach/coachDashboard/coachMessaging/ScheduledMessages.js
+++ coach-me/src/components/coach/coachDashboard/coachMessaging/ScheduledMessages.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import './ScheduledMessages.scss';
 
 function ScheduledMessages(props) {
     const state = useSelector(state => state);
     const dispatch = useDispatch();
+    const [sec, setSec] = useState('');
     const [schedule, setSchedule] = useState({
         msg: '',
         min: '',
@@ -32,6 +34,7 @@ function ScheduledMessages(props) {
             })
             .catch(err => console.log(err));
     };
+    console.log(schedule.sec, 'seconds');
     return (
         <>
             <div>
@@ -48,48 +51,254 @@ function ScheduledMessages(props) {
                     placeholder='Type your message here'
                     required
                 ></textarea>
-
-                <input
-                    type='number'
-                    onChange={handleInputChange}
-                    name='sec'
-                    value={schedule.sec}
-                    placeholder='Enter Seconds'
-                />
-
-                <input
-                    type='number'
-                    onChange={handleInputChange}
-                    name='min'
-                    value={schedule.min}
-                    placeholder='Enter Minutes'
-                />
-                <input
-                    type='number'
-                    onChange={handleInputChange}
-                    name='hour'
-                    value={schedule.hour}
-                    placeholder='Enter Hour in Military Time'
-                />
-                <input
-                    type='number'
-                    onChange={handleInputChange}
-                    name='dom'
-                    value={schedule.dom}
-                    placeholder='Enter Day of Month'
-                />
-                <input
-                    onChange={handleInputChange}
-                    name='month'
-                    value={schedule.month}
-                    placeholder='Enter Month'
-                />
-                <input
-                    onChange={handleInputChange}
-                    name='weekday'
-                    value={schedule.weekday}
-                    placeholder='Enter Weekday'
-                />
+                <div className='selectheader'>
+                    <select
+                        name='sec'
+                        value={schedule.sec}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
+                        <option value={10}>10</option>
+                        <option value={11}>11</option>
+                        <option value={12}>12</option>
+                        <option value={13}>13</option>
+                        <option value={14}>14</option>
+                        <option value={15}>15</option>
+                        <option value={16}>16</option>
+                        <option value={17}>17</option>
+                        <option value={18}>18</option>
+                        <option value={19}>19</option>
+                        <option value={20}>20</option>
+                        <option value={21}>21</option>
+                        <option value={22}>22</option>
+                        <option value={23}>23</option>
+                        <option value={24}>24</option>
+                        <option value={25}>25</option>
+                        <option value={26}>26</option>
+                        <option value={27}>27</option>
+                        <option value={28}>28</option>
+                        <option value={29}>29</option>
+                        <option value={30}>30</option>
+                        <option value={31}>31</option>
+                        <option value={32}>32</option>
+                        <option value={33}>33</option>
+                        <option value={34}>34</option>
+                        <option value={35}>35</option>
+                        <option value={36}>36</option>
+                        <option value={37}>37</option>
+                        <option value={38}>38</option>
+                        <option value={39}>39</option>
+                        <option value={40}>40</option>
+                        <option value={41}>41</option>
+                        <option value={42}>42</option>
+                        <option value={43}>43</option>
+                        <option value={44}>44</option>
+                        <option value={45}>45</option>
+                        <option value={46}>46</option>
+                        <option value={47}>47</option>
+                        <option value={48}>48</option>
+                        <option value={49}>49</option>
+                        <option value={50}>50</option>
+                        <option value={51}>51</option>
+                        <option value={52}>52</option>
+                        <option value={53}>53</option>
+                        <option value={54}>54</option>
+                        <option value={55}>55</option>
+                        <option value={56}>56</option>
+                        <option value={57}>57</option>
+                        <option value={58}>58</option>
+                        <option value={59}>59</option>
+                        <option value={60}>60</option>
+                    </select>
+                    <h1>Select Seconds</h1>
+                </div>
+                <div className='selectheader'>
+                    <select
+                        name='min'
+                        value={schedule.min}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
+                        <option value={10}>10</option>
+                        <option value={11}>11</option>
+                        <option value={12}>12</option>
+                        <option value={13}>13</option>
+                        <option value={14}>14</option>
+                        <option value={15}>15</option>
+                        <option value={16}>16</option>
+                        <option value={17}>17</option>
+                        <option value={18}>18</option>
+                        <option value={19}>19</option>
+                        <option value={20}>20</option>
+                        <option value={21}>21</option>
+                        <option value={22}>22</option>
+                        <option value={23}>23</option>
+                        <option value={24}>24</option>
+                        <option value={25}>25</option>
+                        <option value={26}>26</option>
+                        <option value={27}>27</option>
+                        <option value={28}>28</option>
+                        <option value={29}>29</option>
+                        <option value={30}>30</option>
+                        <option value={31}>31</option>
+                        <option value={32}>32</option>
+                        <option value={33}>33</option>
+                        <option value={34}>34</option>
+                        <option value={35}>35</option>
+                        <option value={36}>36</option>
+                        <option value={37}>37</option>
+                        <option value={38}>38</option>
+                        <option value={39}>39</option>
+                        <option value={40}>40</option>
+                        <option value={41}>41</option>
+                        <option value={42}>42</option>
+                        <option value={43}>43</option>
+                        <option value={44}>44</option>
+                        <option value={45}>45</option>
+                        <option value={46}>46</option>
+                        <option value={47}>47</option>
+                        <option value={48}>48</option>
+                        <option value={49}>49</option>
+                        <option value={50}>50</option>
+                        <option value={51}>51</option>
+                        <option value={52}>52</option>
+                        <option value={53}>53</option>
+                        <option value={54}>54</option>
+                        <option value={55}>55</option>
+                        <option value={56}>56</option>
+                        <option value={57}>57</option>
+                        <option value={58}>58</option>
+                        <option value={59}>59</option>
+                        <option value={60}>60</option>
+                    </select>
+                    <h1>Select Minutes</h1>
+                </div>
+                <div className='selectheader'>
+                    <select
+                        name='hour'
+                        value={schedule.hour}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
+                        <option value={10}>10</option>
+                        <option value={11}>11</option>
+                        <option value={12}>12</option>
+                        <option value={13}>13</option>
+                        <option value={14}>14</option>
+                        <option value={15}>15</option>
+                        <option value={16}>16</option>
+                        <option value={17}>17</option>
+                        <option value={18}>18</option>
+                        <option value={19}>19</option>
+                        <option value={20}>20</option>
+                        <option value={21}>21</option>
+                        <option value={22}>22</option>
+                        <option value={23}>23</option>
+                        <option value={24}>24</option>
+                    </select>
+                    <h1>Select Hour in Military Time</h1>
+                </div>
+                <div className='selectheader'>
+                    <select
+                        name='dom'
+                        value={schedule.dom}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
+                        <option value={10}>10</option>
+                        <option value={11}>11</option>
+                        <option value={12}>12</option>
+                        <option value={13}>13</option>
+                        <option value={14}>14</option>
+                        <option value={15}>15</option>
+                        <option value={16}>16</option>
+                        <option value={17}>17</option>
+                        <option value={18}>18</option>
+                        <option value={19}>19</option>
+                        <option value={20}>20</option>
+                        <option value={21}>21</option>
+                        <option value={22}>22</option>
+                        <option value={23}>23</option>
+                        <option value={24}>24</option>
+                        <option value={25}>25</option>
+                        <option value={26}>26</option>
+                        <option value={27}>27</option>
+                        <option value={28}>28</option>
+                        <option value={29}>29</option>
+                        <option value={30}>30</option>
+                        <option value={31}>31</option>
+                    </select>
+                    <h1>Select Day of Month</h1>
+                </div>
+                <div className='selectheader'>
+                    <select
+                        name='month'
+                        value={schedule.month}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
+                        <option value={10}>10</option>
+                        <option value={11}>11</option>
+                        <option value={12}>12</option>
+                    </select>
+                    <h1>Select Month</h1>
+                </div>
+                <div className='selectheader'>
+                    <select
+                        name='weekday'
+                        value={schedule.weekday}
+                        onChange={handleInputChange}
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                    </select>
+                    <h1>Select Weekday</h1>
+                </div>
                 <input
                     onChange={handleInputChange}
                     name='numbers'
