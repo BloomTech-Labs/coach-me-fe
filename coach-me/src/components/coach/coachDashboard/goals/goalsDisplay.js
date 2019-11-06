27c95de933c4a637dba72fd93deb55162d68841a
Pat Palmerston <patpalmerston@gmail.com>
2019-11-06 07:07:06 -0800

    goals display component recieiving goals state from redux

--- coach-me/src/components/coach/coachDashboard/goals/goalsDisplay.js
+++ coach-me/src/components/coach/coachDashboard/goals/goalsDisplay.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGoals } from '../../../../actions/coachActions';
+import GoalsDisplayModal from './GoalDisplayModal';
+import GoalCard from './GoalCard';
 
-const goalsDisplay = () => {
-    return <div>I am the GoalsDisplay Component</div>;
+const GoalsDisplay = props => {
+    const state = useSelector(state => state.coach);
+    const dispatch = useDispatch();
+
+    const { clientprofile } = props;
+
+    useEffect(() => {
+        if (clientprofile && clientprofile.clientId) {
+            dispatch(getGoals(clientprofile.clientId));
+        }
+    }, [clientprofile]);
+
+    console.log('GoalsDisplay Component State', state);
+    console.log('GoalsDisplay Component props', props);
+    return (
+        <div>
+            <h1>I am the GoalsDisplay Component</h1>
+            <GoalCard />
+            <GoalsDisplayModal />
+        </div>
+    );
 };
 
-export default goalsDisplay;
+export default GoalsDisplay;
