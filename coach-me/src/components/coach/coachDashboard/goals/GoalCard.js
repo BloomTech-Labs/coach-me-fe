b9a2b44b0ba969228deb2faa725e8c0eb61c7102
Pat Palmerston <patpalmerston@gmail.com>
2019-11-06 15:39:06 -0800

    creating class names for buttons

--- coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
+++ coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './goalCard.scss';
 import moment from 'moment';
 const GoalCard = props => {
-    console.log('GoalCards', props.goal);
+    // console.log('GoalCards', props.goal);
     return (
         <div className='text-container'>
-            <p>Start Date: {moment(props.startDate).format('MMM Do')}</p>
+            <h6>Start Date: {moment(props.startDate).format('MMM Do')}</h6>
             <p>{props.goal}</p>
         </div>
     );
