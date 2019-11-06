53d9d5bc418b917185c399ebc9028742c0e46092
Pat Palmerston <patpalmerston@gmail.com>
2019-11-06 15:06:23 -0800

    updated button

--- coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
+++ coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './goalCard.scss';
+import moment from 'moment';
 const GoalCard = props => {
     console.log('GoalCards', props.goal);
     return (
         <div className='text-container'>
-            <p>Start Date: {props.startDate}</p>
+            <p>Start Date: {moment(props.startDate).format('MMM Do')}</p>
             <p>{props.goal}</p>
         </div>
     );
