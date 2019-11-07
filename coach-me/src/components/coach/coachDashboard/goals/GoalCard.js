d1a25d331f4060b85fba01c0f59e6874c80a841f
Pat Palmerston <patpalmerston@gmail.com>
2019-11-07 14:42:21 -0800

    update goal card to fix bugs

--- coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
+++ coach-me/src/components/coach/coachDashboard/goals/GoalCard.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import './goalCard.scss';
 import moment from 'moment';
+import { ReactComponent as Arrow } from '../assets/Vector.svg';
+import { ReactComponent as GreenCheck } from '../assets/greenCheckMark.svg';
+import { ReactComponent as RedX } from '../assets/redX.svg';
 
 const GoalCard = props => {
-    // console.log('GoalCards', props.goal);
-    return (
-        <div className='text-container'>
-            <h6>Start Date: {moment(props.startDate).format('MMM Do')}</h6>
-            <>
-                Met Goal? <input type='checkbox' checked='checked'></input>
-            </>
-            <div className='arrow collapsed'>
-                {/* <FontAwesomeIcon icon={faCheveronUp} />
-                <FontAwesomeIcon icon={faCheveronDown} /> */}
+    console.log('GoalCards', props);
+    let goalMet;
+    let goalResponse;
+    if (props.metGoal === 'Yes') {
+        goalMet = <GreenCheck />;
+        goalResponse = <p style={{ color: 'green' }}>Met Goal</p>;
+    } else {
+        goalMet = <RedX />;
+        goalResponse = <p style={{ color: 'red' }}>Didn't Meet Goal</p>;
+    }
+
+    if (props.goal === undefined) {
+        return null;
+    } else {
+        return (
+            <div className='goal-text-container'>
+                <div className='goal-mas'>
+                    <div className='goalMet'>{goalMet}</div>
+                    <p>{goalResponse}</p>
+                </div>
+                <div className='text-container'>
+                    <div className='start-date'>
+                        Start Date: {moment(props.startDate).format('MMM Do')}
+                    </div>
+                    <div className='goal-text'>
+                        <p>{props.goal}</p>
+                    </div>
+                </div>
+                <div className='arrow'>
+                    <Arrow />
+                </div>
             </div>
-            <p>{props.goal}</p>
-        </div>
-    );
+        );
+    }
 };
 
 export default GoalCard;
