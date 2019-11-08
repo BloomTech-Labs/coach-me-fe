90c5c68b7c1be42ad1e7a0005f64c5d75e15d162
jjgorton <jjgorton@gmail.com>
2019-11-08 00:07:28 -0500

    logout functionality, toggle menu, fixed header

--- coach-me/src/components/coach/coachDashboard/CoachHeader.js
+++ coach-me/src/components/coach/coachDashboard/CoachHeader.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './coachHeader.scss';
 import coachMeIcon from '../../utils/assets/1coachHeader.svg';
 import Coach from '../../utils/assets/2coachHeader.svg';
 import Me from '../../utils/assets/3coachHeader.svg';
-import downArrow from '../../utils/assets/downArrow.svg';
+import { ReactComponent as DownArrow } from '../../utils/assets/downArrow.svg';
+import { ReactComponent as UpArrow } from '../../utils/assets/upArrow.svg';
 import { useSelector } from 'react-redux';
 
-const CoachHeader = () => {
+const CoachHeader = props => {
     const state = useSelector(state => state.auth);
-    console.log('state', state);
+    const [coachName, setCoachName] = useState('');
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        setCoachName(state.coachName);
+    }, []);
+    console.log('STATE', state);
+
+    const toggleLogout = () => {};
+
+    const logout = () => {
+        localStorage.clear();
+        props.history.push('/login');
+    };
+
     return (
         <div className='coach-header'>
             <div className='header-icon'>
@@ -21,12 +36,22 @@ const CoachHeader = () => {
                 <img className='Me' alt='Me' src={Me}></img>
             </div>
             <div className='small-profile'>
-                <h3>{state.coachName}</h3>
-                <img
-                    className='downArrow'
-                    alt='downArrow'
-                    src={downArrow}
-                ></img>
+                <h3>{coachName}</h3>
+
+                <DownArrow
+                    onClick={e => setToggle(true)}
+                    className={`arrow ${toggle ? 'hide' : null}`}
+                />
+                <UpArrow
+                    onClick={e => setToggle(false)}
+                    className={`arrow ${!toggle ? 'hide' : null}`}
+                />
+                <button
+                    onClick={e => logout()}
+                    className={`menu ${!toggle ? 'hide' : null}`}
+                >
+                    Logout
+                </button>
             </div>
         </div>
     );
