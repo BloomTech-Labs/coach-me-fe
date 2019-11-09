1de84340072cc11a61f00bf7363b5eefe13f278a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-11-09 11:47:14 -0500

    Added tests to GoalsDisplay to check if modal works

---
+++ coach-me/cypress/integration/GoalsDisplay.spec.js
@@ -0,0 +1,24 @@
+window.localStorage.setItem(
+    'token',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2FjaElkIjoicmVjWXdEOWVuTUc0bjJ4cUQiLCJjb2FjaE5hbWUiOiJLYXJpbiBVbmRlcndvb2QiLCJpYXQiOjE1NzMzMTY5MTMsImV4cCI6MTU3MzQwMzMxM30.ajc3EF7tbiAgQ5bh1e-CD8czt-NOs94TXF7pv71HQaA'
+);
+
+Cypress.Commands.add('login', uid => {
+    window.localStorage.setItem(
+        'token',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2FjaElkIjoicmVjWXdEOWVuTUc0bjJ4cUQiLCJjb2FjaE5hbWUiOiJLYXJpbiBVbmRlcndvb2QiLCJpYXQiOjE1NzMzMTY5MTMsImV4cCI6MTU3MzQwMzMxM30.ajc3EF7tbiAgQ5bh1e-CD8czt-NOs94TXF7pv71HQaA'
+    );
+    window.localStorage.setItem('coachName', 'Karin Underwood');
+    cy.visit('/dashboard');
+});
+
+describe('User can navigate homepage', () => {
+    before(() => {
+        cy.login();
+    });
+
+    it('Modal Clicks', () => {
+        cy.get('.goals-wrapper-button').click();
+        cy.get('.modal-button').click();
+    });
+});
