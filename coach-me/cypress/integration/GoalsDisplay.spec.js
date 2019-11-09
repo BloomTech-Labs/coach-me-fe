db786d4144b0dfdae15ca1d5a441a332cb5de427
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-11-09 11:49:56 -0500

    Added test to SearchForm component to check if input works

--- coach-me/cypress/integration/GoalsDisplay.spec.js
+++ coach-me/cypress/integration/GoalsDisplay.spec.js
@@ -1,8 +1,3 @@
-window.localStorage.setItem(
-    'token',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2FjaElkIjoicmVjWXdEOWVuTUc0bjJ4cUQiLCJjb2FjaE5hbWUiOiJLYXJpbiBVbmRlcndvb2QiLCJpYXQiOjE1NzMzMTY5MTMsImV4cCI6MTU3MzQwMzMxM30.ajc3EF7tbiAgQ5bh1e-CD8czt-NOs94TXF7pv71HQaA'
-);
-
 Cypress.Commands.add('login', uid => {
     window.localStorage.setItem(
         'token',
@@ -14,7 +9,7 @@ Cypress.Commands.add('login', uid => {
 
 describe('User can navigate homepage', () => {
     before(() => {
-        cy.login();
+        cy.coachlogin();
     });
 
     it('Modal Clicks', () => {
