a705354ac5bf80b35408967b6ae6defb4014c986
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-11-09 11:00:22 -0500

    Added input and route tests to CoachRegistration component

---
+++ coach-me/cypress/integration/CoachRegistration.spec.js
@@ -0,0 +1,26 @@
+// Tests if all inputs accepting text input from user
+describe('Input Test', () => {
+    it('Accepts input', () => {
+        const text = 'Perfect';
+        cy.visit('/register');
+        cy.get('[data-cy=input1]')
+            .type(text)
+            .should('have.value', text);
+        cy.get('[data-cy=input2]')
+            .type(text)
+            .should('have.value', text);
+        cy.get('[data-cy=input3]')
+            .type(text)
+            .should('have.value', text);
+    });
+});
+
+describe('Route Test', () => {
+    it('Tests if page routes correctly', () => {
+        cy.visit('/register');
+
+        cy.get('.login').click();
+
+        cy.url().should('include', '/login');
+    });
+});
