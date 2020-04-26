a2413edd27aec855ee95733ac95454c0401fcff3
jarrodskahill <jarrodskahill@gmail.com>
2020-04-25 17:10:51 -0700

    started create profile

---
+++ coach-me/src/components/clients/onboarding/CreateProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './createProfile';
+
+const CreateProfile = () => {
+    return (
+        <div>
+            <h1 className='profile-header'>Let's create your profile</h1>
+        </div>
+    );
+};
+
+export default CreateProfile;
