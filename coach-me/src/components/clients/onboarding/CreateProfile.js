6dc62daeef276705a19672731dc58d3f0446ffa2
jarrodskahill <jarrodskahill@gmail.com>
2020-04-28 19:29:20 -0700

    test

--- coach-me/src/components/clients/onboarding/CreateProfile.js
+++ coach-me/src/components/clients/onboarding/CreateProfile.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import './createProfile';
+import React, { useState } from "react";
+import "./createProfile.scss";
 
 const CreateProfile = () => {
+    const [config, setConfig] = useState({
+        weight: "",
+        height: "",
+        gender: false,
+    });
+    const handleChange = (e) => {
+        setConfig({ ...config, [e.target.name]: e.target.value });
+    };
     return (
-        <div>
-            <h1 className='profile-header'>Let's create your profile</h1>
+        <div className="create-profile">
+            <h1 className="profile-header">Let's create your profile</h1>
+            <input
+                handleChange={handleChange}
+                placeholder="Weight"
+                className="create-input"
+            />
+            <input
+                handleChange={handleChange}
+                placeholder="Height"
+                className="create-input"
+            />
+            <div className="gender-profile">
+                <div></div>
+            </div>
         </div>
     );
 };
