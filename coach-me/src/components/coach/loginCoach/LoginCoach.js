cea202414f28c184cc3e4d61a18b20780d738c99
HumphreyJ <joshhumphrey1@gmail.com>
2020-05-14 20:36:19 -0500

    Refactoring Coach Login component for code climate score...i mean maintainability.

--- coach-me/src/components/coach/loginCoach/LoginCoach.js
+++ coach-me/src/components/coach/loginCoach/LoginCoach.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { loginCoach } from '../../../actions/authActions';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../utils/assets/coachmelogo-white.svg';
+import CoachLoginForm from './CoachLoginForm';
 
 //Component Imports
 import Modal from './Modal';
@@ -11,37 +12,30 @@ import Modal from './Modal';
 import './loginCoach.scss';
 
 const LoginCoach = props => {
-    const [email, setemail] = useState();
-    const [password, setpassword] = useState();
+    const [coachCredentials, setCoachCredentials] = useState({
+        email: '',
+        password: ''
+    });
     const dispatch = useDispatch();
-    const [creds, setCreds] = useState({ email: '', password: '' });
+
     const [modal, setModal] = useState(false);
 
     const triggerModal = () => {
         setModal(true);
     };
 
-    const handleChange2 = e => {
+    const handleChange = e => {
         e.preventDefault();
-        setemail(e.target.value);
-    };
-    const handleChange3 = e => {
-        e.preventDefault();
-
-        setpassword(e.target.value);
-    };
-
-    useEffect(() => {
-        setCreds({
-            email: email,
-            password: password
+        setCoachCredentials({
+            ...coachCredentials,
+            [e.target.name]: e.target.value
         });
-    }, [email, password]);
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
         localStorage.clear();
-        dispatch(loginCoach(creds)).then(() => {
+        dispatch(loginCoach(coachCredentials)).then(() => {
             props.history.push('/dashboard');
         });
     };
@@ -67,57 +61,12 @@ const LoginCoach = props => {
                 <div className='side-two'>
                     <h1>Login</h1>
                     <p>Welcome back! Please login to your coach account.</p>
-                    <form
-                        className='Login-Form-Wrapper'
-                        onSubmit={e => {
-                            handleSubmit(e);
-                        }}
-                    >
-                        <div className='input-Wrapper'>
-                            <div className='input-layout'>
-                                <label>Email</label>
-                                <input
-                                    data-cy='input1'
-                                    type='text'
-                                    placeholder='Email'
-                                    name='E-Mail'
-                                    className='email'
-                                    onChange={handleChange2}
-                                    value={email}
-                                />
-                            </div>
-                            <div className='input-layout'>
-                                <label>Password</label>
-                                <input
-                                    data-cy='input2'
-                                    type='password'
-                                    placeholder='Password'
-                                    name='password'
-                                    className='password'
-                                    onChange={handleChange3}
-                                    value={password}
-                                />
-                                <div
-                                    className='forgot'
-                                    onClick={() => triggerModal()}
-                                >
-                                    Forgot password?
-                                </div>
-                            </div>
-                        </div>
-                        <div className='layout-wrapper'>
-                            <button type='submit' className='signup-btn'>
-                                {' '}
-                                Login
-                            </button>
-                            <div className='register-container'>
-                                Don't have an account?{' '}
-                                <Link className='register' to='/register'>
-                                    Sign up
-                                </Link>
-                            </div>
-                        </div>
-                    </form>
+                    <CoachLoginForm
+                        handleChange={handleChange}
+                        handleSubmit={handleSubmit}
+                        coachCredentials={coachCredentials}
+                        triggerModal={triggerModal}
+                    />
                 </div>
             </div>
         </>
