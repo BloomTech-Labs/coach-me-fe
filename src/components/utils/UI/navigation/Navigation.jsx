bf57a703fd5e22c31fb9ecb2ca1da1d77e746917
Trey McGarity <trey.mcgarity99@gmail.com>
2020-05-22 11:08:03 -0500

    merge complications

--- src/components/utils/UI/navigation/Navigation.jsx
+++ src/components/utils/UI/navigation/Navigation.jsx
@@ -1,16 +1,12 @@
-import React,{useContext} from 'react';
+import React from 'react';
 import { ReactComponent as Logo } from '../.././assets/coachmelogo-white.svg';
-import UIContext from'../../context/UIContext';
 import './navigation.scss';
 
 const Navigation = () => {
-    const { backdropHandler, drawerOpen } = useContext(UIContext)
     return (
         <nav className='navigation'>
             <Logo />
-            <i 
-            onClick={backdropHandler}
-            className={drawerOpen ? 'fas fa-bars fa-3x rotate' : "fas fa-bars fa-3x"}></i>
+            <i className="fas fa-bars fa-3x"></i>
         </nav>
     );
 }
--- src/components/utils/helpers.js
+++ src/components/utils/helpers.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from 'react'
+import { Provider } from 'react-redux'
 import store from '../../store';
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import Show from '../clients/assets/show_password.png';
+import Hide from '../clients/assets/hide_password.png';
 
 export const Helper = ui => {
-    return {
-        ...render(
-            <Provider store={store}>
-                <BrowserRouter>{ui}</BrowserRouter>
-            </Provider>
-        ),
-        store
-    };
-};
+  return {
+    ...render(
+      <Provider store={store}>
+          <BrowserRouter>{ui}</BrowserRouter>
+      </Provider>
+    ),
+    store,
+  }
+}
