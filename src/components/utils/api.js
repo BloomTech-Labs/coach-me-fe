9be92f4f8cb0b41abc7929c6ec9422c93716749c
Jesse-Tingle <jtingle0@gmail.com>
2020-06-01 13:34:37 -0400

    update to image size on home page and update to make api.js a function

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-export default axios.create({
-    baseURL: process.env.REACT_APP_BACKEND,
+import axios from "axios";
 
-})
+export default function () {
+	axios.defaults.withCredentials = true;
+	return axios.create({
+		baseURL: process.env.REACT_APP_BACKEND,
+	});
+}
