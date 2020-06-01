f3f1701da0f6b4c915bd28172c33035dbd8e3971
Jesse-Tingle <jtingle0@gmail.com>
2020-06-01 14:03:11 -0400

    cleans up some react warnings

--- src/components/clients/clientDashboard/ResourceCenter.jsx
+++ src/components/clients/clientDashboard/ResourceCenter.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import './clientDashboard.scss';
+import React from "react";
+import "./clientDashboard.scss";
 
 const ResourceCenter = () => {
-    return (
-        <div className='wrapper'>
-            <h1>Resource Center</h1>
-            <div className='container'>
-                 <p>You have no resources at this time!</p>
-            </div>
-        </div>
-    );
+	return (
+		<div className="wrapper">
+			<h1>Resource Center</h1>
+			<div className="container">
+				<p>You have no resources at this time!</p>
+			</div>
+		</div>
+	);
 };
 
 export default ResourceCenter;
--- src/components/utils/UI/navigation/Navigation.jsx
+++ src/components/utils/UI/navigation/Navigation.jsx
@@ -1,37 +1,42 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
-import React,{useContext} from 'react';
-import { Link } from 'react-router-dom'
-
-import { ReactComponent as Logo } from '../.././assets/coachmelogo-white.svg';
-import './navigation.scss';
-import { FormikProvider } from 'formik';
-import api from '../../api';
+import { ReactComponent as Logo } from "../.././assets/coachmelogo-white.svg";
+import "./navigation.scss";
+import { FormikProvider } from "formik";
+import api from "../../api";
+import UIContext from "../../context/UIContext";
 
 const Navigation = () => {
+	const { backdropHandler, drawerOpen } = useContext(UIContext);
+	const logout = () => {
+		localStorage.clear();
 
-    const { backdropHandler, drawerOpen } = useContext(UIContext)
-    const logout = () => {
-        localStorage.clear()
-        
-        api.post(`${process.env.REACT_APP_BACKEND}/auth/logout`, {withCredentials: true})
-    }
-    return (
-        <nav className='navigation'>
-            <Logo />
-            <div className='nav-links'>
-                <Link to='/'>Home</Link>
-                <Link to='/dashboard'>Dashboard</Link>
-                <Link to='/'>Support</Link>
-                <Link
-                onClick={logout}
-                to='/'>Logout</Link>
-            </div>
-            <i 
-            onClick={backdropHandler}
-            className={drawerOpen ? 'fas fa-bars fa-3x rotate' : "fas fa-bars fa-3x"}></i>
-
-        </nav>
-    );
-}
+		api.post(`${process.env.REACT_APP_BACKEND}/auth/logout`, {
+			withCredentials: true,
+		});
+	};
+	return (
+		<nav className="navigation">
+			<Logo />
+			<div className="nav-links">
+				<Link to="/">Home</Link>
+				<Link to="/dashboard">Dashboard</Link>
+				<Link to="/">Support</Link>
+				<Link onClick={logout} to="/">
+					Logout
+				</Link>
+			</div>
+			<i
+				onClick={backdropHandler}
+				className={
+					drawerOpen
+						? "fas fa-bars fa-3x rotate"
+						: "fas fa-bars fa-3x"
+				}
+			></i>
+		</nav>
+	);
+};
 
 export default Navigation;
