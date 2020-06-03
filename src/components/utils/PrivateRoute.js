1705e680fef91b4bb3ed0ec5e5660ac21bfcd46a
brianetaveras <taveras574@gmail.com>
2020-06-02 21:47:55 -0400

    Added route guard. It works yo, it works!

--- src/components/utils/PrivateRoute.js
+++ src/components/utils/PrivateRoute.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AxiosWithCred from './axiosWithCred';
 import { Route, Redirect } from 'react-router';
 
-const PrivateRoute = async ({ component: Component, ...rest }) => {
-    const verified = await AxiosWithCred.get('/auth/verify_session');
+const PrivateRoute =  ({ component: Component, ...rest }) => {
+    const [auth, setAuth] = useState(false);
+    useEffect(async ()=>{
+        try {
+            const verified = await AxiosWithCred.get('/auth/verify_session');
+            setAuth(true)
+        } catch (error) {
+            setAuth(false)
+        }
+    }, [])
     return (
         <Route
             {...rest}
             render={props => {
-                if ( verified ) {
+                if ( auth ) {
                     return <Component {...props} />;
                 }
                 return <Redirect to='/' />;
