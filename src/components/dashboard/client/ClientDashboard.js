9b008171de022fe862c385ea9c69a2f661022fb6
Trey McGarity <trey.mcgarity99@gmail.com>
2020-06-10 18:18:29 -0500

    updated routes and started new client dashboard

--- src/components/dashboard/client/ClientDashboard.js
+++ src/components/dashboard/client/ClientDashboard.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
-import Notifications from "./Notifications";
-import ResourceCenter from "./ResourceCenter";
-import SessionNotes from "./SessionNotes";
-import HealthMetric from "../client/health_metrics/HealthMetric";
+// import Notifications from "./Notifications";
+// import ResourceCenter from "./ResourceCenter";
+// import SessionNotes from "./SessionNotes";
+// import HealthMetric from "../client/health_metrics/HealthMetric";
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getClientInfo } from "../../../redux/actions/clientActions";
 import "../../../sass/dashboard/client/clientDashboard.scss";
 
@@ -13,17 +14,29 @@ const ClientDashboard = (props) => {
 	useEffect(() => {
 		dispatch(getClientInfo());
 	}, []);
-	const state = useSelector((state) => state.client);
+	// const state = useSelector((state) => state.client);
 
 	return (
 		<div className="client-dashboard">
-			<div className="profile-container">
-				{<h4>Welcome, {props.state.first_name}!</h4>}
+			<div className="tabs-container">
+				<div className="notifications">
+				<Link className="tab top" to="client-notifications">Notifications</Link>
+				<div className="notification-count">5</div>
+				</div>
+				<Link className="tab" to="resource-center">Resource Center</Link>
+				<Link className="tab" to="coach-messages">Coach Messages</Link>
+				<Link className="tab" to="track-progess">Track Progress</Link>
 			</div>
-			<Notifications />
-			<ResourceCenter />
-			<SessionNotes />
-			<HealthMetric />
+			<div className="profile-container">
+				<div className="profile">
+					{<h1>{props.state.first_name}</h1>}
+					<div className="condition diabetes">Diabetes</div>
+					<div className="condition hbp">HBP</div>
+				</div>
+				<div className="motivation">
+
+				</div>
+			</div>
 		</div>
 	);
 };
