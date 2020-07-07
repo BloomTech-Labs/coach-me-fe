6088c49fa6e08dfa3eb8a69bedbff0b5b05a1317
HumphreyJ <joshhumphrey1@gmail.com>
2020-07-06 19:06:48 -0500

    corrected a small styling error on image circle.

--- src/components/login/client/LoginClient.test.js
+++ src/components/login/client/LoginClient.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Helper as render } from "../../utils/helpers";
+import { Helper as render } from "../../../utils/helpers";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import LoginClient from "./LoginClient";
