697dc4c384967d99922adafb7dbeb6c3d154bb2c
Trey McGarity <trey.mcgarity99@gmail.com>
2020-07-21 18:54:57 -0500

    got to 40 percent test coverage for frontend

--- src/components/dashboard/client/ClientDashboard.test.js
+++ src/components/dashboard/client/ClientDashboard.test.js
@@ -3,7 +3,7 @@ import { Helper as render } from "../../../utils/helpers";
 import "@testing-library/jest-dom/extend-expect";
 import ClientDashboard from "./ClientDashboard";
 
-describe("ClientDashboard component and texts", () => {
+describe("ClientDashboard component", () => {
 	it("Renders without crashing", async () => {
 		render(<ClientDashboard />);
 	})
